35d82607a8a8a4d128a4df8cedbd53fc020ba3ce
acesso-unico <alessandra.portela.2025@outlook.com>
2026-01-12 17:01:37 -0300

    .

--- js/store-config.js
+++
@@ -1,9 +0,0 @@
-// Copie este arquivo para cada loja publicada e ajuste os valores abaixo.
-// Exemplo: window.LOJA_SLUG = 'olympikus-corrida';
-//          window.LOJA_DOMAIN = 'https://lojaexemplo.com';
-(function(){
-    window.LOJA_SLUG = window.LOJA_SLUG || 'defina-o-slug-da-sua-loja';
-    window.LOJA_DOMAIN = window.LOJA_DOMAIN || window.location.origin;
-    window.TRACK_ENDPOINT = window.TRACK_ENDPOINT || 'https://blackfystore.com/app/api/track_visitor.php';
-    window.TRACK_API_KEY = window.TRACK_API_KEY || 'dfy_tracker_ba2725f6c9f54e2c8f8f1cdc2b7ac9a3';
-})();
